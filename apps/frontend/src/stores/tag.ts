2abe40380d8d36aed45bdad0d04432edaa6e8a6e
tyhf <3125502636@qq.com>
2026-07-08 12:46:43 +0800

    添加turbo 统一脚本管理 完全配置完pc前端可运行 之后更改后端+配置提交规范和钩子

--- apps/frontend/src/stores/tag.ts
+++ apps/frontend/src/stores/tag.ts
@@ -1,35 +1,35 @@
-import { defineStore } from 'pinia';
-import { ref } from 'vue';
-import type { RouteLocationNormalizedLoadedGeneric } from 'vue-router';
-import { useRouter } from 'vue-router';
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import type { RouteLocationNormalizedLoadedGeneric } from 'vue-router'
+import { useRouter } from 'vue-router'
 
 export const useTagStore = defineStore('tag', () => {
-  const router = useRouter();
-  const tagList = ref<RouteLocationNormalizedLoadedGeneric[]>([]);
+  const router = useRouter()
+  const tagList = ref<RouteLocationNormalizedLoadedGeneric[]>([])
   const addTag = (tag: RouteLocationNormalizedLoadedGeneric) => {
     if (!tagList.value.some((item) => item.path === tag.path)) {
-      tagList.value.push(tag);
+      tagList.value.push(tag)
     }
-  };
+  }
 
   const closeTag = (path: string, routePath: string) => {
-    const agIndex: number = tagList.value.findIndex((item) => item.path === path);
+    const agIndex: number = tagList.value.findIndex((item) => item.path === path)
     if (agIndex !== -1) {
-      tagList.value.splice(agIndex, 1);
+      tagList.value.splice(agIndex, 1)
 
       if (path === routePath && tagList.value.length > 1) {
-        const pushTag = tagList.value[agIndex] ? agIndex : agIndex - 1;
+        const pushTag = tagList.value[agIndex] ? agIndex : agIndex - 1
 
-        const targetTag = tagList.value[pushTag];
+        const targetTag = tagList.value[pushTag]
         if (targetTag) {
-          router.push(targetTag.path);
+          router.push(targetTag.path)
         }
       }
     }
-  };
+  }
   return {
     tagList,
     addTag,
     closeTag,
-  };
-});
+  }
+})
